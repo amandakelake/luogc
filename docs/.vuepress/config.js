5763aadc50d39096b7fb486f3d73c2a737177307
皛白 <xiaobai@fordeal.com>
2020-06-19 14:36:49 +0800

    docs: update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,17 +10,17 @@ module.exports = {
         logo: 'https://s3.forcloudcdn.com/dmc/d6d10ad7-0eab-4f24-a458-36178179231e-323x323.png',
         // 添加导航栏
         nav: [
-            { text: 'JS基础', link: '/js-basic' },
-            { text: '框架', link: '/framework/' },
+            { text: 'JS基础', link: '/js-basic/this' },
+            { text: '框架', link: '/framework/vue' },
             { text: '工程化', link: '/engineering/travis' },
-            { text: '关于我', link: '/about-me' },
+            { text: '关于我', link: '/about-me/me' },
             { text: 'Github', link: 'https://github.com/amandakelake'}
         ],
         sidebar: [
             {
-                title: 'Group 1',   // 必要的
-                path: '/foo/',      // 可选的, 应该是一个绝对路径
-                collapsable: false, // 可选的, 默认值是 true,
+                title: 'JavaScript基础',   // 必要的
+                // path: '/foo/',      // 可选的, 应该是一个绝对路径
+                collapsable: true, // 可选的, 默认值是 true,
                 sidebarDepth: 1,    // 可选的, 默认值是 1
                 children: [
                     '/',
@@ -29,8 +29,20 @@ module.exports = {
                 ]
             },
             {
-                title: 'Group 2',
-                children: [ /* ... */ ]
+                title: '浏览器原理',
+                children: ['/browser/browser']
+            },
+            {
+                title: '框架相关',
+                children: ['/framework/vue']
+            },
+            {
+                title: '解决方案',
+                children: ['/solutions/skeleton']
+            },
+            {
+                title: '工程化',
+                children: ['/engineering/travis']
             }
         ]
     },
